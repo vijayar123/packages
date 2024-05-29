c9961b098b58e022ffa0d556a70eee0d80db75af
vijayar123 <vijay.a@ciglobalsolutions.com>
2024-05-29 13:40:22 +0530

    inital commit

---
+++ packages/lib/flutter_package_ci.dart
@@ -0,0 +1,15 @@
+// library flutter_package_ci;
+//
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+// class ConnectivityService {
+//   Future<bool> isConnected() async {
+//     var connectivityResult = await Connectivity().checkConnectivity();
+//     return connectivityResult != ConnectivityResult.none;
+//   }
+// }
